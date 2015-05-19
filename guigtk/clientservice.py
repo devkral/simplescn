2900773e6831f76f96613f692dbe888b36698f65
alex <devkral@web.de>
2015-05-19 20:51:05 +0200

    service gui

--- guigtk/clientservice.py
+++ guigtk/clientservice.py
@@ -9,13 +9,15 @@ class gtkclient_remoteservice(gtkclient_template):
     name=None
     def __init__(self,links,_address,dparam,name=""):
         gtkclient_template.__init__(self,sharedir+"guigtk/clientservice.ui",links,_address,dparam)
-        self.name=name
-        #self.get_object("col1").set_orientation(Gtk.Orientation.VERTICAL)
         self.win=self.get_object("servicewin")
-        self.win.set_visible(True)
         self.win.set_title(name)
         self.update()
         
-    def update(self):
+    def update(self,*args):
+        pass
+    
+    def copy(self,*args):
         pass
         
+    def close(self,*args):
+        self.win.destroy()
