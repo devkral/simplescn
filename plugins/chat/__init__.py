d98cd7803467284e0861b4c529aff5bfcf9184bd
Alex <devkral@web.de>
2015-10-06 12:31:39 +0200

    improve chat

--- plugins/chat/__init__.py
+++ plugins/chat/__init__.py
@@ -1,6 +1,8 @@
 
 from gi.repository import Gtk, Gdk
 from threading import Lock
+#import os
+import os.path
 
 ###### created by pluginmanager ######
 # specifies the interfaces
@@ -61,20 +63,21 @@ gui_server_actions=[{"text":"foo-actionname","action":sampleaction, "icon":"opti
 #    pass
 #    return widget
 
-def send_text(_address, certhash, _text):
-    sock, _cert, _hash = resources("plugin")(_address, "chat", "send_text", forcehash=certhash)
+def send_text(_address, certhash, _text, private_state):
+    _text = bytes(_text, "utf-8")
+    sock, _cert, _hash = resources("plugin")(_address, "chat", "{}/send_text/{}".format(private_state, len(_text)), forcehash=certhash)
     if sock is None:
         return
-    sock.send(bytes(_text, "utf-8"))
+    sock.send(_text)
     sock.close()
 
-def gtk_send_text(widget, _address, certhash):
+def gtk_send_text(widget, _address, certhash, private_state):
     _text = widget.get_text()
-    send_text(_address, certhash, _text)
+    send_text(_address, certhash, _text, private_state)
     widget.set_text("")
     with chatlock[certhash]:
         _oldlineno = chatbuf[certhash].get_line_count()
-        chatbuf[certhash].insert(chatbuf[certhash].get_end_iter(), "me: "+_text+"\n")
+        chatbuf[certhash].insert(chatbuf[certhash].get_end_iter(), _text+"\n")
         _newiter = chatbuf[certhash].get_end_iter()
         chatbuf[certhash].apply_tag_by_name("ownposts", chatbuf[certhash].get_iter_at_line(_oldlineno-1), _newiter)
 
@@ -89,7 +92,7 @@ def gui_node_iface(gui, _name, _hash, _address):
         chatbuf[_hash].create_tag("ownposts", justification=Gtk.Justification.RIGHT, justification_set=True, foreground_rgba=Gdk.RGBA(red=0.0, green=1.0, blue=0.9, alpha=1.0), foreground_set=True)
     chatgrid = Gtk.Grid()
     chatin = Gtk.Entry()
-    chatin.connect("activate", gtk_send_text, _address, _hash)
+    chatin.connect("activate", gtk_send_text, _address, _hash, "public")
     chattview = Gtk.TextView(buffer=chatbuf[_hash], editable=False, hexpand=True, vexpand=True)
     
     chatswin = Gtk.ScrolledWindow(child=chattview, hexpand=True, vexpand=True)
@@ -102,11 +105,22 @@ def gui_node_iface(gui, _name, _hash, _address):
 ### uncomment for being accessable by internet
 ### client:
 def receive(action, _socket, _cert, certhash):
+    splitted = action.split("/",2)
+    if len(splitted)!=3:
+        return
+    private, action, _size = splitted
     with chatlock[certhash]:
         if action == "send_text":
-            _text = _socket.read(1500)
-            chatbuf[certhash].insert(chatbuf[certhash].get_end_iter(), str(_text, "utf-8")+"\n")
-            
+            if private != "private":
+                #logob = open (os.path.join(config.get("logdir"), certhash), "a")
+                logob = None
+            else:
+                logob = None
+            _text = str(_socket.read(int(_size)), "utf-8")+"\n"
+            chatbuf[certhash].insert(chatbuf[certhash].get_end_iter(), _text)
+            if logob is not None:
+                logob.write(_text)
+                logob.close()
     
         
 ## executed when redirected, return False, when redirect should not be executed
