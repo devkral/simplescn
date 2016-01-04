1fd02185b5937c84f7fdccf6ad94a1a833596efd
Alex <devkral@web.de>
2016-01-04 18:07:13 +0100

    add test stubs

---
+++ tests/testall.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python3
+
+import testserver, testcommon
+
+if __name__ == "main":
+    pass
---
+++ tests/testcommon.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+
+import unittest
+
+def do_tests():
+    pass
+
+if __name__ == "main":
+    do_tests
