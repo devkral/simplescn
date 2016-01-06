59ff2b347d2711feab2e2d1c6901663382d49770
Alex <devkral@web.de>
2016-01-07 00:21:38 +0100

    try to fix setup, to include all files

--- setup.py
+++ setup.py
@@ -3,13 +3,20 @@
 from setuptools import setup, find_packages
 
 setup(name='simplescn',
-      version='0.1',
-      description='Simple communication nodes',
-      author='Alex',
-      author_email='devkral@web.de',
-      url='https://github.com/devkral/simplescn',
-      scripts = ['client.py', 'guiclient.py', 'server.py'],
-      license = "BSD3",
-      test_suite = "tests"
-      
+    version='0.1',
+    description='Simple communication nodes',
+    author='Alex',
+    author_email='devkral@web.de',
+    url='https://github.com/devkral/simplescn',
+    scripts = ['client.py', 'guiclient.py', 'server.py', 'common.py'],
+    include_package_data = True,
+    package_data = {
+        # If any package contains *.ui files, include them:
+        '': ['*.ui', '*.py'],
+    },
+    packages = find_packages(),
+    
+    license = "BSD3",
+    test_suite = "tests"
+    
      )
