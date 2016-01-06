63a0dab35d776f400d9fb93fda2fffcbb67fa65a
Alex <devkral@web.de>
2016-01-06 23:26:12 +0100

    fix import path, move plugins in simplescn, add missing setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='simplescn',
+      version='0.1',
+      description='Simple communication nodes',
+      author='Alex',
+      author_email='devkral@web.de',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['', ''],
+     )
--- tests/testcommon.py
+++ tests/testcommon.py
@@ -3,7 +3,7 @@
 import sys
 import os
 # fix import
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "simplescn"))
 
 
 import unittest
