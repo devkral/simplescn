931e9d48e45597356bb46993ed69226df2f5db4e
Alex <devkral@web.de>
2016-02-02 19:33:59 +0100

    check if dependencies are available

--- simplescn.py
+++ simplescn.py
@@ -4,18 +4,25 @@
 """
 startscript for simplescn
 """
+import sys
 
-# load for nuitkabuild
+# load for nuitkabuild also a good test if available
 try:
     import markdown
 except ImportError:
-    pass
+    print("No markdown support", file=sys.stderr)
 
 try:
     import gi
 except ImportError:
-    pass
+    print("No gtk gui support", file=sys.stderr)
 
+# required:
+try:
+    import cryptography
+except ImportError:
+    print("Error: no cryptography", file=sys.stderr)
+    sys.exit(1)
 from simplescn import __main__
 if __name__ == "__main__":
     __main__.init_method_main()
