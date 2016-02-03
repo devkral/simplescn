7ca7e89e4192948599055d81c72047122fafb099
Alex <devkral@web.de>
2016-02-03 11:45:02 +0100

    setup can be forced to install gui

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@
 #license: bsd3, see LICENSE.txt
 
 from setuptools import setup
+
+relaxed = False
+forcegui = True
 #, Distribution
 #from pkg_resources import Environment, working_set
 
@@ -14,6 +17,18 @@ from setuptools import setup
 #    print("Error loading plugins: ", errors)
 #print(distributions)
 
+
+entry_points = {}
+install_requirements = ["cryptography>=1.1"]
+if forcegui:
+    entry_points["gui_scripts"] = ['simplescns = simplescn.__main__:init_method_main']
+    install_requirements += ["simplescn[gtkgui]", "simplescn[mdhelp]"]
+elif not relaxed:
+    entry_points["console_scripts"] = ['simplescns = simplescn.__main__:init_method_main']
+    install_requirements += ["simplescn[mdhelp]"]
+else:
+    entry_points["console_scripts"] = ['simplescns = simplescn.__main__:init_method_main']
+
 # plugins imported by MANIFEST.in
 setup(name='simplescn',
       version='0.1',
@@ -21,21 +36,17 @@ setup(name='simplescn',
       author='Alexander K.',
       author_email='devkral@web.de',
       url='https://github.com/devkral/simplescn',
-      #scripts=['simplescns.py'],
-      entry_points={
-        'console_scripts': [
-            'simplescns = simplescn.__main__:init_method_main'
-            ]
-        },
+      entry_points=entry_points,
       #zip_safe=True,
+      platforms='Platform Independent',
       include_package_data=True,
       package_data={
           'simplescn': ['*.txt', '*.md', 'guigtk/*.ui', 'guigtk/*.svg', 'guigtk/*.py', 'static/*', 'html/*/*.html'],
       },
-      install_requires=["cryptography>=1.1", "simplescn"],
+      install_requires=install_requirements,
       extras_require={
-          'gtkgui': ["pygobject"],
-          'mdhelp': ["markdown"],
+          'gtkgui': ["pygobject>=3.16"],
+          'mdhelp': ["markdown>=2.0"],
       },
       packages=['simplescn'],
       #ext_modules=distributions,
